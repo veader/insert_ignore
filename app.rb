31e4b6ebc73590fcfce4a31f3876150a914f2dda
Shawn Veader <shawn@veader.org>
2013-04-25 12:00:25 -0400

    handle multiple rows in the select output

--- app.rb
+++ app.rb
@@ -9,19 +9,33 @@ end
 post '/' do
   @output = \
     begin
-      generate_insert_query(params[:table_name], params[:select_result])
+      generate_sql(params[:table_name], params[:select_result])
     rescue Exception => ex
       ex.message
-    end
+    end.gsub(/\n/, '<br><br>')
 
   erb :results
 end
 
 
 # ---------------------------------------------------------------------------
-def generate_insert_query(table_name, input)
-  columns = []
-  values  = []
+def generate_sql(table_name, input)
+
+  columns, value_rows = find_columns_and_value_rows(input)
+
+  raise 'Invalid input received, try again.' if columns.empty? || value_rows.empty?
+  # make sure we have the same number of columns and values
+  raise 'Column and value counts do not match.' if columns.count != value_rows.first.count
+
+  queries = \
+  value_rows.collect { |row| generate_insert_query(table_name, columns, row) }
+
+  queries.join("\n")
+end
+
+def find_columns_and_value_rows(input)
+  columns    = []
+  value_rows = []
 
   # look through input for our columns and values
   input.split("\n").each do |line|
@@ -34,18 +48,18 @@ def generate_insert_query(table_name, input)
     if row.first == 'id'
       columns = row
     else
-      values  = row
+      value_rows << row
     end
   end
 
-  raise 'Invalid input received, try again.' if columns.empty? || values.empty?
-  # make sure we have the same number of columns and values
-  raise 'Column and value counts do not match.' if columns.count != values.count
+  [columns, value_rows]
+end
 
+def generate_insert_query(table, columns, values)
   columns_str = columns.join(', ')
   values_str = \
     values.collect { |v| needs_quotes?(v) ? "\"#{v}\"" : v }.join(', ')
-  "INSERT IGNORE INTO #{table_name} (#{columns_str}) VALUES (#{values_str})"
+  "INSERT IGNORE INTO #{table} (#{columns_str}) VALUES (#{values_str});"
 end
 
 def needs_quotes?(value)
