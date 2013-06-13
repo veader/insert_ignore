c2f082502383a4892020b62e15badd6ccd1a0800
Shawn Veader <shawn@veader.org>
2013-06-13 18:02:45 -0400

    beginnings of being able to parse and display vertical output

--- app.rb
+++ app.rb
@@ -17,20 +17,44 @@ post '/' do
   erb :results
 end
 
+VERTICAL_LINE_REGEX=/^\s+(.+?)\:\s(.+)$/
 
 # ---------------------------------------------------------------------------
 def generate_sql(table_name, input)
+  queries = process_input(table_name, input)
+  queries.join("\n")
+end
 
+def process_input(table_name, input)
+  if in_vertical_format?(input)
+    process_vertical_format(table_name, input)
+  else
+    process_standard_format(table_name, input)
+  end
+end
+
+def in_vertical_format?(input)
+  logger.info 'in_vertical_format?'
+  # in the first four lines you should encounter a line that has the format
+  # http://rubular.com/r/Pg5eIwsMZ9
+  input.split("\n")[0,4].detect do |line|
+    line =~ VERTICAL_LINE_REGEX
+  end
+end
+
+def process_vertical_format(table_name, input)
+  ["That's in \\G format. We are working on support for it." +
+   "Until then try standard select format output."]
+end
+
+def process_standard_format(table_name, input)
   columns, value_rows = find_columns_and_value_rows(input)
 
   raise 'Invalid input received, try again.' if columns.empty? || value_rows.empty?
   # make sure we have the same number of columns and values
   raise 'Column and value counts do not match.' if columns.count != value_rows.first.count
 
-  queries = \
   value_rows.collect { |row| generate_insert_query(table_name, columns, row) }
-
-  queries.join("\n")
 end
 
 def find_columns_and_value_rows(input)
